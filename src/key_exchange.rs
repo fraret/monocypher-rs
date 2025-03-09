7241582310de40fa04389c1aea11f0290ebc6e03
Sergi Soler 'fraret <fraretblauet@gmail.com>
2025-03-09 20:49:26 +0100

    Added X25519 DH key-exchange

--- src/key_exchange.rs
+++ src/key_exchange.rs
@@ -22,6 +22,15 @@ pub fn public(secret_key: [u8; 32]) -> [u8; 32] {
     }
 }
 
+pub fn x25519(your_secret_key: [u8; 32], their_public_key: [u8; 32]) -> [u8; 32] {
+    unsafe {
+        let mut raw_shared_secret = mem::MaybeUninit::<[u8;32]>::uninit();
+        ffi::crypto_x25519(raw_shared_secret.as_mut_ptr() as *mut u8, your_secret_key.as_ptr(),
+                           their_public_key.as_ptr());
+        raw_shared_secret.assume_init()
+    }
+}
+
 #[cfg(test)]
 mod test {
     use crate::key_exchange;
@@ -38,5 +47,19 @@ mod test {
                 2, 150, 133, 5, 13, 52, 71, 69, 186, 5, 135, 14, 88, 125, 89
             ]
         )
+    }
+
+    #[test]
+    fn dh1() {
+        let secret_key_1 = [2u8; 32];
+        let public_key_1 = key_exchange::public(secret_key_1);
+
+        let secret_key_2 = [5u8; 32];
+        let public_key_2 = key_exchange::public(secret_key_2);
+
+        let shared_secret_1 = key_exchange::x25519(secret_key_1, public_key_2);
+        let shared_secret_2 = key_exchange::x25519(secret_key_2, public_key_1);
+
+        assert_eq!(shared_secret_1, shared_secret_2);
     }
 }
